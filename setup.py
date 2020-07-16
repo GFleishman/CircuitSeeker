f1dc63b6dcba73635d21886358a7252ecb55d062
fleishmang <fleishmang@janelia.hhmi.org>
2020-07-16 13:53:22 -0400

    added dependencies and package requirements to setup

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuptools.setup(
     author="Greg M. Fleishman",
     author_email="greg.nli10me@gmail.com",
     description="Tools for finding neural circuits",
+    url="https://github.com/GFleishman/CircuitSeeker",
     license="MIT",
     packages=setuptools.find_packages(),
     install_requires=[
