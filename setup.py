6d3c78448d933d84db0c32e6a5ee400f7aeffbfa
fleishmang <fleishmang@janelia.hhmi.org>
2021-03-30 08:59:07 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="CircuitSeeker",
-    version="0.0.4",
+    version="0.1.0",
     author="Greg M. Fleishman",
     author_email="greg.nli10me@gmail.com",
     description="Tools for finding neural circuits",
